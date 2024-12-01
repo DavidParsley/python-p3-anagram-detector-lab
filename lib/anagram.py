63598f7a13e8bde44c0cb57adcc9947ba586dc25
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-12-02 00:17:21 +0300

    adds anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagrams):
+        sort_words =sorted(self.word)
+        return[anagram for anagram in anagrams if sort_words == sorted(anagram)]    
+    
+listen = Anagram("cat")
+grams = listen.match(['act', 'atc', 'cat', 'tca', 'cta', 'tac'])
+
+print(grams)     
